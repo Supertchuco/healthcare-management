b2f49aebe98f316abb2becac4d73fff92111f3b5
Rafael <rafael.whatsthestory@gmail.com>
2019-07-11 02:29:25 -0300

    fix annotation in repositories classes and remove empty lines in build.gradle file

--- src/main/java/com/healthcare/healthcaremanagement/repository/ExamRepository.java
+++ src/main/java/com/healthcare/healthcaremanagement/repository/ExamRepository.java
@@ -2,7 +2,9 @@ package com.healthcare.healthcaremanagement.repository;
 
 import com.healthcare.healthcaremanagement.entity.Exam;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExamRepository extends CrudRepository<Exam, Integer> {
 
     Exam findById(int examId);
--- src/main/java/com/healthcare/healthcaremanagement/repository/ProcedureRepository.java
+++ src/main/java/com/healthcare/healthcaremanagement/repository/ProcedureRepository.java
@@ -2,7 +2,9 @@ package com.healthcare.healthcaremanagement.repository;
 
 import com.healthcare.healthcaremanagement.entity.Procedure;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProcedureRepository extends CrudRepository<Procedure, Integer> {
 
     Procedure findByName(String name);
